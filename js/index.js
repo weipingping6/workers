d0c1ff77d076bad2bd571e50ee44e07fda002e67
weipingping6 <kedawp@163.com>
2016-12-15 17:07:30 +0800

    second

--- js/index.js
+++ js/index.js
@@ -2,20 +2,33 @@
 
 init();
 //初始化
-function init(){
+function init(){	
+	addbg();
 	var tabs=$("wrap-l").getElementsByTagName("li");
 	for(var i=0;i<tabs.length;i++){
-		tabs[i].iself=i;
+		tabs[i].iself=i;		
 		tabs[i].onclick=function(){
-			tab(this.iself)
+			tab(this.iself);
 		}
 	}
+	
 }
 //通过id获取元素
 function $(id){
 	var ele=document.getElementById(id);
 	return ele;
 }
+//给每个item元素加上对应的背景
+function addbg(){
+	var items=$("wrap-r").getElementsByTagName("div");
+	for(var i=0;i<items.length;i++){
+		var itemsli=items[i].getElementsByTagName("li");
+		for(var j=0;j<itemsli.length;j++){
+			itemsli[j].style.backgroundImage="url(img/"+i+"-"+j+".jpg)";
+		}
+	}
+}
+
 function tab(tar){
 	var items=$("wrap-r").getElementsByTagName("div");
 	for(var i=0;i<items.length;i++){
